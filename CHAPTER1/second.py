047d55f4ea9326453dc1354caf24d0f589d3867f
DhruvJha707 <dhruvjha707@gmail.com>
2026-01-29 18:56:18 +0530

    Initial commit

---
+++ CHAPTER1/second.py
@@ -0,0 +1,7 @@
+#check whether the number is multiple of 5 or not
+num = int(input("Enter a number: "))
+
+if num % 5 == 0:
+    print("The number is divisible by 5")
+else:
+    print("The number is not divisible by 5")    
